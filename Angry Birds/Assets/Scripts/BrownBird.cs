ab8d2b4b42aa823034cbf62f87173096a96fc96a
Madbrotherz <61371417+Madbrotherz@users.noreply.github.com>
2020-03-26 00:01:34 +0700

    Commit & Push Project

---
+++ Angry Birds/Assets/Scripts/BrownBird.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrownBird : Bird
+{
+    [SerializeField]
+    public GameObject explosionObj;
+
+    public void Update()
+    {
+        if (State == BirdState.HitSomething)
+        {
+            GameObject childObject = Instantiate(explosionObj) as GameObject;
+            childObject.transform.parent = gameObject.transform;
+        }
+    }
+
+}
---
+++ Angry Birds/Assets/Scripts/GameController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public SlingShooter SlingShooter;
+    public TrailController TrailController;
+    public List<Bird> Birds;
+    public List<Enemy> Enemies;
+    private Bird _shotBird;
+    private bool _isGameEnded = false;
+    public BoxCollider2D TapCollider;
+    public UIControllerScript uiScript;
+
+    void Start()
+    {
+        uiScript = FindObjectOfType<UIControllerScript>();
+        for (int i = 0; i < Birds.Count; i++)
+        {
+            Birds[i].OnBirdDestroyed += ChangeBird;
+            Birds[i].OnBirdShot += AssignTrail;
+        }
+
+        for (int i = 0; i < Enemies.Count; i++)
+        {
+            Enemies[i].OnEnemyDestroyed += CheckGameEnd;
+        }
+
+        TapCollider.enabled = false;
+        SlingShooter.InitiateBird(Birds[0]);
+        _shotBird = Birds[0];
+    }
+
+    void Update()
+    {
+        if (Birds.Contains(null))
+        {
+            Debug.Log("NExt");
+        }
+    }
+
+    public void ChangeBird()
+    {
+        TapCollider.enabled = false;
+
+        if (_isGameEnded)
+        {
+            return;
+        }
+
+        Birds.RemoveAt(0);
+
+        if (Birds.Count > 0)
+            SlingShooter.InitiateBird(Birds[0]);
+            _shotBird = Birds[0];
+    }
+
+    public void CheckGameEnd(GameObject destroyedEnemy)
+    {
+        for (int i = 0; i < Enemies.Count; i++)
+        {
+            if (Enemies[i].gameObject == destroyedEnemy)
+            {
+                Enemies.RemoveAt(i);
+                break;
+            }
+        }
+
+        if (Enemies.Count == 0)
+        {
+            Debug.Log("Game Ended");
+            _isGameEnded = true;
+            uiScript.endGame();
+        }
+    }
+
+    public void AssignTrail(Bird bird)
+    {
+        TrailController.SetBird(bird);
+        StartCoroutine(TrailController.SpawnTrail());
+        TapCollider.enabled = true;
+    }
+
+    void OnMouseUp()
+    {
+        if (_shotBird != null)
+        {
+            _shotBird.OnTap();
+        }
+    }
+}
